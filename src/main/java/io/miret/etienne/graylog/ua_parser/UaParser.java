5094907d39e07581eba2cf943c60035093fe7196
Etienne Miret <etienne.miret@ens-lyon.org>
2018-10-24 23:02:57 +0200

    Implement User-Agent string parsing.

--- src/main/java/io/miret/etienne/graylog/ua_parser/UaParser.java
+++ src/main/java/io/miret/etienne/graylog/ua_parser/UaParser.java
@@ -1,12 +1,25 @@
 package io.miret.etienne.graylog.ua_parser;
 
+import org.graylog2.plugin.Message;
 import org.graylog2.plugin.Messages;
 import org.graylog2.plugin.messageprocessors.MessageProcessor;
+import ua_parser.Client;
+import ua_parser.Parser;
+
+import javax.inject.Inject;
 
 public class UaParser implements MessageProcessor {
 
+  @Inject
+  private Parser parser;
+
   @Override
   public Messages process (Messages messages) {
+    for (Message message : messages) {
+      String agent = message.getFieldAs (String.class, "agent");
+      Client client = parser.parse (agent);
+      message.addField ("client", client);
+    }
     return messages;
   }
 
