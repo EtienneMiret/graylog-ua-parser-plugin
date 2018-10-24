c04012430fade6fdda5b0f9de19125053b4a900c
Etienne Miret <etienne.miret@ens-lyon.org>
2018-10-24 21:10:55 +0200

    Reformat entire project.

--- src/main/java/io/miret/etienne/graylog/UaParserModule.java
+++ src/main/java/io/miret/etienne/graylog/UaParserModule.java
@@ -6,20 +6,20 @@ import org.graylog2.plugin.PluginModule;
  * Extend the PluginModule abstract class here to add you plugin to the system.
  */
 public class UaParserModule extends PluginModule {
-    @Override
-    protected void configure() {
-        registerPlugin(UaParserMetadata.class);
+  @Override
+  protected void configure () {
+    registerPlugin (UaParserMetadata.class);
 
-        /* Register your plugin types here.
-         *
-         * Examples:
-         *
-         * addMessageInput(Class<? extends MessageInput>);
-         * addMessageFilter(Class<? extends MessageFilter>);
-         * addMessageOutput(Class<? extends MessageOutput>);
-         * addPeriodical(Class<? extends Periodical>);
-         * addAlarmCallback(Class<? extends AlarmCallback>);
-         * addInitializer(Class<? extends Service>);
-         */
-    }
+    /* Register your plugin types here.
+     *
+     * Examples:
+     *
+     * addMessageInput(Class<? extends MessageInput>);
+     * addMessageFilter(Class<? extends MessageFilter>);
+     * addMessageOutput(Class<? extends MessageOutput>);
+     * addPeriodical(Class<? extends Periodical>);
+     * addAlarmCallback(Class<? extends AlarmCallback>);
+     * addInitializer(Class<? extends Service>);
+     */
+  }
 }
