116cff7defda75c05d57499b9025c677b737f4a4
Etienne Miret <etienne.miret@ens-lyon.org>
2018-10-25 18:32:17 +0200

    Flatten client fields.
    It seems me that graylog do not support nested fields.

--- src/main/java/io/miret/etienne/graylog/ua_parser/UaParser.java
+++ src/main/java/io/miret/etienne/graylog/ua_parser/UaParser.java
@@ -17,8 +17,31 @@ public class UaParser implements MessageProcessor {
   public Messages process (Messages messages) {
     for (Message message : messages) {
       String agent = message.getFieldAs (String.class, "agent");
-      Client client = parser.parse (agent);
-      message.addField ("client", client);
+      if (agent != null) {
+        Client client = parser.parse (agent);
+        if (client.userAgent != null) {
+          String version = String.format ("%s.%s.%s",
+              client.userAgent.major, client.userAgent.minor, client.userAgent.patch);
+          message.addField ("client.userAgent.family", client.userAgent.family);
+          message.addField ("client.userAgent.version", version);
+          message.addField ("client.userAgent.major", client.userAgent.major);
+          message.addField ("client.userAgent.minor", client.userAgent.minor);
+          message.addField ("client.userAgent.patch", client.userAgent.patch);
+        }
+        if (client.os != null) {
+          String version = String.format ("%s.%s.%s",
+              client.os.major, client.os.minor, client.os.patch);
+          message.addField ("client.os.family", client.os.family);
+          message.addField ("client.os.version", version);
+          message.addField ("client.os.major", client.os.major);
+          message.addField ("client.os.minor", client.os.minor);
+          message.addField ("client.os.patch", client.os.patch);
+          message.addField ("client.os.patchMinor", client.os.patchMinor);
+        }
+        if (client.device != null) {
+          message.addField ("client.device.family", client.device.family);
+        }
+      }
     }
     return messages;
   }
